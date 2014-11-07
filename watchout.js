5e4b8cb4a57c47802acaeac7127f169b2a017ea4
bethjohnson <bethjohnson3@gmail.com>
2014-11-07 14:42:53 -0800

    Drew the enemies in an svg element.

--- watchout.js
+++ watchout.js
@@ -1,2 +1,32 @@
 // start slingin' some d3 here.
 
+var width = 1000,
+    height = 600;
+var svg = d3.select("body").append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+var enemiesNum = 15;
+var enemiesArr = [];
+
+for (var i=0; i < enemiesNum; i++){
+  var x = Math.random()*width;
+  var y = Math.random()*height;
+
+  enemiesArr.push({id : i, x : x, y : y});
+
+}
+
+
+svg.selectAll("body")
+  .data(enemiesArr)
+  .enter()
+  .append('svg:circle')
+  .attr('cx', function(d){
+    return d.x;
+  })
+  .attr('cy', function(d){
+    return d.y;
+  })
+  .attr('r', 10);
+
